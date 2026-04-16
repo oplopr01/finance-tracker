bd13bf0c4779aaa5100bd889b80f8fe36e2ceb2c
oplopr01 <oplopr01@gmail.com>
2026-04-16 21:10:44 +0530

    added BE URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import toast from "react-hot-toast";
-
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 // ✅ Attach token to every request
