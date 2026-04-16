0de397095e999c0794faff20fc5064e9e690398f
oplopr01 <oplopr01@gmail.com>
2026-04-16 20:46:39 +0530

    for vercel trigger

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -107,7 +107,6 @@ const sortedTransactions = [
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
-
       <div className="p-6 max-w-4xl mx-auto">
 
         {/* Summary */}
